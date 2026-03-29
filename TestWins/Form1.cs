d4e3d3ef7bf69345c9a187352c3ae95a948defda
Lix <lynsymariesoronio05@gmail.com>
2026-03-29 10:47:05 +0800

    completed button functionalities

--- TestWins/Form1.cs
+++ TestWins/Form1.cs
@@ -6,7 +6,6 @@ namespace TestWins;
 public partial class Form1 : Form
 {
     //business
-
     private readonly StudentController controller = new StudentController();
     public Form1()
     {
@@ -21,23 +20,45 @@ public partial class Form1 : Form
 
     private void btnAdd_Click(object sender, EventArgs e)
     {
-
+        var student = new TestWins.Model.Student
+        {
+            studentId = txtStudentId.Text,
+            Name = txtName.Text,
+            age = int.Parse(txtAge.Text),
+            course = txtCourse.Text
+        };
+        controller.createStudent(student);
+        loadData();
     }
 
     private void btnUpdate_Click(object sender, EventArgs e)
     {
-
+        var student = new TestWins.Model.Student
+        {
+            studentId = txtStudentId.Text,
+            Name = txtName.Text,
+            age = int.Parse(txtAge.Text),
+            course = txtCourse.Text
+        };
+        controller.update(student);
+        loadData();
     }
 
     private void btnDelete_Click(object sender, EventArgs e)
     {
-
-
-
+        controller.delete(txtStudentId.Text);
+        loadData();
     }
 
     private void dataGridView1_CellClick(object sender, EventArgs e)
     {
-
+        if (dataGridView1.CurrentRow != null)
+        {
+            DataGridViewRow row = dataGridView1.CurrentRow;
+            txtStudentId.Text = row.Cells[0].Value.ToString();
+            txtName.Text = row.Cells[1].Value.ToString();
+            txtAge.Text = row.Cells[2].Value.ToString();
+            txtCourse.Text = row.Cells[3].Value.ToString();
+        }
     }
 }
